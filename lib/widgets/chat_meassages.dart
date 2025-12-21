5ac28d539fff23fb06bb7efa7ce7b28cc009a488
RonydaEssam <ronyda988@gmail.com>
2025-12-21 03:26:44 +0200

    fetching messages from firebase firestore

--- lib/widgets/chat_meassages.dart
+++ lib/widgets/chat_meassages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,42 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Text('No messages found'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Center(
+            child: const Text('No messages found'),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Something went wrong!'));
+        }
+
+        final loadedMeassages = snapshot.data!.docs;
+
+        return ListView.builder(
+          padding: EdgeInsets.only(bottom: 40, left: 13, right: 13),
+          reverse: true,
+          itemCount: loadedMeassages.length,
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Text(loadedMeassages[index].data()['text']),
+            );
+          },
+        );
+      },
     );
   }
 }
